1f0f9109b2e4713a75caf927604718e80a670a16
Hui4401 <Hui4401@qq.com>
2023-12-24 16:10:58 +0800

    pb

---
+++ error/base.go
@@ -0,0 +1,11 @@
+package error
+
+type errorCode = int32
+
+// 通用错误码 0-1000
+const (
+	CodeOk      errorCode = 0
+	CodeUnknown errorCode = -1
+
+	CodeParam errorCode = 1 // 请求参数错误
+)
---
+++ error/user.go
@@ -0,0 +1,8 @@
+package error
+
+// user错误码 1000-2000
+const (
+	CodeUserExist     errorCode = 1001
+	CodeUserNotExist  errorCode = 1002
+	CodePasswordError errorCode = 1003
+)
