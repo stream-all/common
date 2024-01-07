a256c88b501ad24baea83e9d8ce269a4b73299f7
Hui4401 <Hui4401@qq.com>
2024-01-07 14:59:55 +0800

    WithCode

---
+++ pb/util/base_resp.go
@@ -0,0 +1,20 @@
+package util
+
+import (
+	"github.com/stream-all/common/pb"
+	"github.com/stream-all/common/pb/error"
+)
+
+func BaseResp(code int32, msg string) *pb.BaseResp {
+	return &pb.BaseResp{
+		Code: code,
+		Msg:  msg,
+	}
+}
+
+func ParseBaseResp(baseResp *pb.BaseResp) (int32, string) {
+	if baseResp == nil {
+		return error.CodeUnknown, "baseResp is nil"
+	}
+	return baseResp.Code, baseResp.Msg
+}
